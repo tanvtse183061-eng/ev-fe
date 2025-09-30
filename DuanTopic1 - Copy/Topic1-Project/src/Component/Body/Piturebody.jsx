28de73ab62d1fa22a6719309af12df3345d99223
tanvtse183061-eng <tanvtse183061@fpt.edu.vn>
2025-09-30 14:44:09 +0700

    sua va them anh xe

--- DuanTopic1 - Copy/Topic1-Project/src/Component/Body/Piturebody.jsx
+++ DuanTopic1 - Copy/Topic1-Project/src/Component/Body/Piturebody.jsx
@@ -1,7 +1,9 @@
 import anhnenherio from './herio.png';
 import anhnenlimo from './limo.png';
 import anhnenvf6_xanh_duong from  './vf6_xanh_duong.png';
-
+import anhnenminio from './minio.png'
+import vf3_vang from './vf3_vang.png'
+import vf7_xam from './vf7_xam.png'
 export default function body(){
     return(
         <div className="body">
@@ -24,6 +26,24 @@ export default function body(){
                     <p className='name-car'>VinFast VF 6</p>
                     <p className='price-car'>GIÁ TỪ 689,000,000 ₫ </p>
                       </div>
+            </div>
+            <div className='car-body2'>
+                  <div className='minio'>
+                    <img src={anhnenminio}/>
+                    <p className='name-car'>Minio Green</p>
+                    <p className='price-car'>GIÁ TỪ 269,000,000 ₫ </p>
+                    </div>
+              <div className='vf7'>
+                    <img src={vf7_xam} />
+                    <p className='name-car'>VinFast VF 7</p>
+                    <p className='price-car'>GIÁ TỪ 799,000,000 ₫ </p>
+                </div> 
+                <div className='vf3'>
+                    <img src={vf3_vang} />
+                    <p className='name-car'>VinFast VF 3</p>
+                    <p className='price-car'>GIÁ TỪ 269,000,000 ₫ </p>
+                    </div>     
+
             </div>
             
             
